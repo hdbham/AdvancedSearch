ee1ae4f0b510f875a43861d40d4aa28c15c73c18
hdbham <hunter.d.burningham@gmail.com>
2022-12-15 15:54:19 -0700

    forgot to change function calls to new name

--- advancedSearch.js
+++ advancedSearch.js
@@ -52,7 +52,7 @@ const thesaurus = [{
 
 const getInputExclusions = (exclusions) => {
   exclusions.forEach((exclusion) => {
-    exclusionSynonyms(exclusion)
+    GetExclusionSynonyms(exclusion)
   });
 };
 
